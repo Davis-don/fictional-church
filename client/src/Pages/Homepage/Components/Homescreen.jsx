68e65568742620a0513dc4ff17ea884c1f3644d4
Davis_the_don <davismugoikou@gmail.com>
2024-10-23 20:05:03 +0300

    arrow edited

--- client/src/Pages/Homepage/Components/Homescreen.jsx
+++ client/src/Pages/Homepage/Components/Homescreen.jsx
@@ -1,16 +1,68 @@
-import React from 'react'
-import './Homepage.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Carosel from './Hero/Carosel/Carosel'
+import React, { useRef } from 'react';
+import './Homepage.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Carosel from './Hero/Carosel/Carosel';
+import { FaArrowDown } from "react-icons/fa";
 
 function Homescreen() {
+  
+  const aboutSectionRef = useRef(null);
+
+
+  const scrollToAboutSection = () => {
+    if (aboutSectionRef.current) {
+      aboutSectionRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <div className='ovearall-homescreen-container'>
-    <div className="homepage-hero-section">
-    <Carosel/>
-    </div>
+    <div className='overall-homescreen-container'>
+      <div className="homepage-hero-section">
+        <div className="carosel-container">
+          <Carosel />
+        </div>
+
+        
+        <div className="scroll-btn" onClick={scrollToAboutSection}>
+          <FaArrowDown className='actual-btn-arrow' />
+        </div>
       </div>
-  )
+
+      
+      <div className="about-section" ref={aboutSectionRef}>
+        
+      </div>
+    </div>
+  );
 }
 
-export default Homescreen
+export default Homescreen;
+
+
+
+// import React from 'react'
+// import './Homepage.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import Carosel from './Hero/Carosel/Carosel'
+// import { FaArrowDown } from "react-icons/fa";
+
+// function Homescreen() {
+//   return (
+//     <div className='ovearall-homescreen-container'>
+//     <div className="homepage-hero-section">
+//       <div className="carosel-container">
+//       <Carosel/>
+//       </div>
+    
+//     <div className="scroll-btn"><FaArrowDown className='actual-btn-arrow'/></div>
+//     </div>
+
+
+//     <div className="about-section">
+
+//     </div>
+//       </div>
+//   )
+// }
+
+// export default Homescreen
