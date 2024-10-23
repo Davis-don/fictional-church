fcc8874dd6ab1d8ebf13d6749f4cb35e9f76f142
Davis_the_don <davismugoikou@gmail.com>
2024-10-23 15:06:18 +0300

    added scroll functionality

--- client/src/Pages/Homepage/Components/Homescreen.jsx
+++ client/src/Pages/Homepage/Components/Homescreen.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
-import Header from '../../../Utilities/Header/Header'
-import './Homepage.css'
-import Carosel from './Carosel/Carosel'
-import Abouthomepg from './Aboutsection/Abouthomepg'
+import React, { useRef } from 'react';  // Combined the imports
+import Header from '../../../Utilities/Header/Header';
+import './Homepage.css';
+import Carosel from './Carosel/Carosel';
+import Abouthomepg from './Aboutsection/Abouthomepg';
+import { FaArrowDown } from "react-icons/fa";
 
 function Homescreen() {
+  const scrollRef = useRef(null);
+
+  const handleScroll = () => {
+    scrollRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div className='overall-homapage-container'>
       <div className="header-hero-container">
-      <div className="header-homepage">
-      <Header/>
-      </div>
-      <div className="carosel-homepage">
-    <Carosel/>
-      </div>
+        <div className="header-homepage">
+          <Header />
+        </div>
+        <div className="carosel-homepage">
+          <Carosel />
+        </div>
+        <div className="scroll-icon-container">
+          <FaArrowDown className='arrow-down-icon' onClick={handleScroll} />
+        </div>
       </div>
 
-      <div className="about-homepage-container">
-      <Abouthomepg/>
+      <div ref={scrollRef} className="about-homepage-container">
+        <Abouthomepg />
       </div>
-        
-        
-        </div>
-  )
+    </div>
+  );
 }
 
-export default Homescreen
+export default Homescreen;
