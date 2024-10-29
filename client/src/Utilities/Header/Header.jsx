df73e225f9c2ffe30b7a192e4b3e051c3e54a8d2
Davis-don <davismugoikou@gmail.com>
2024-10-29 16:37:33 +0300

    made changes

--- client/src/Utilities/Header/Header.jsx
+++ client/src/Utilities/Header/Header.jsx
@@ -7,46 +7,33 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div className="overall-header-section">
-      <div className="header-content-holders container-fluid">
-        <div className="logo-name-left">
-          <div className="logo-image">
-            <img src={logo} alt="Jesus Christ" className="rounded-circle" />
-          </div>
-          <div className="brand-name">
-            <h1>Fictional Church</h1>
-          </div>
+    <div className="overall-header-container">
+      <div className="left-header">
+     <div className="logo-icon">
+      <img src={logo} alt="gurdian angel" className="rounded-circle"/>
+     </div>
+     <div className="church-name">
+       <h1 className="logo-name">Guardian Angels Tetu Catholic Parish</h1>
+     </div>
+      </div>
+      <div className="right-header">
+        <div className="menu-icon">
+          <CiMenuBurger className="actual-menu-icon"/>
         </div>
-        <div className="logo-name-right">
-          <ul className="list-unstyled nav-links-ul">
-          <li className="link-item"><Link  to = '/'>HOME</Link></li>
-            <li className="link-item"><Link to = '/about'>ABOUT</Link></li>
-            <li className="link-item"><Link to = '/sermons'>SERMONS</Link></li>
-            <li className="link-item"><Link to = '/events'>EVENTS</Link></li>
-            <li className="link-item"><Link to = '/churches'>CHURCHES</Link></li>
-            <li className="link-item"><Link to = '/contact'>CONTACT</Link></li>
-            
-            <li>
-              <button
-                style={{
-                  fontSize: "2rem",
-                  backgroundImage: "var(---btn-color-gradient)",
-                }}
-                className="btn login-btn text-light"
-                type="button"
-              >
-                Login
-              </button>
-            </li>
-          </ul>
-
-          <div className="menu-icon">
-            <button className="btn" type="button">
-              Login
-            </button>
-            <CiMenuBurger className="actual-menu-icon" />
-          </div>
+        <div className="nav-links">
+        <ul className="list-unstyled">
+     <li><Link to = '/'>HOME</Link></li>
+     <li><Link to = '/about'>ABOUT</Link></li>
+     <li><Link to = '/sermons'>SERMONS</Link></li>
+     <li><Link to = '/events'>EVENTS</Link></li>
+     <li><Link to = '/churches'>CHURCHES</Link></li>
+     <li><Link to = '/contact'>CONTACT</Link></li>
+     </ul>
         </div>
+        <div className="login-btn">
+       <button className="btn ">Login</button>
+        </div>
+     
       </div>
     </div>
   );
