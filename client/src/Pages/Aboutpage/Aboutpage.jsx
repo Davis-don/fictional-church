53590a7e453ded2430ee9686bf76258df7aea2b7
Davis-don <davismugoikou@gmail.com>
2024-10-29 21:46:44 +0300

    added history section

--- client/src/Pages/Aboutpage/Aboutpage.jsx
+++ client/src/Pages/Aboutpage/Aboutpage.jsx
@@ -3,10 +3,12 @@ import './aboutpage.css'
 import Essence from './Essence/Essence'
 import Hero from '../../Utilities/Hero/Hero'
 import abthero from '../../assets/footer.jpg'
+import History from './History/History'
 function Aboutpage() {
   return (
     <div className='overall-aboutpg-container'>
         <Hero page="About" imgsrc = {abthero}/>
+        <History/>
         <Essence/>
         </div>
   )
