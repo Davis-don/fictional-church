b2fda458bd1c36c573ba0f45fbdcd77e8b1a51d8
Davis-don <davismugoikou@gmail.com>
2024-10-30 16:29:38 +0300

    sermon section

---
+++ client/src/Pages/Sermons/Daysermon.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './daysermon.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import imagesermon from '../../assets/swahili.jpg'
+function Daysermon() {
+  return (
+    <div className='oveerall-sermonday-section'>
+        <h1>LATEST SERMON</h1>
+        <p className='text-secondary day-p'>A discourse for the purpose of religious</p>
+        <div className="day-sermon-image">
+            <img src={imagesermon} alt='bible image'/>
+        </div>
+        <div className="sermon-text-box">
+            <h2 className='sermon-title'>WHAT JESUS SEES IN YOU</h2>
+            <p className='preacher-detail'>Pastor: <span className='text-danger'>Josh Brighton</span> <br/>
+             On: 08 MAR 2018,
+             </p>
+             <p className='text-secondary sermon-content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at convallis justo. Suspendisse ut nisl diam. Sed non vehicula sem. Etiam efficitur eros a leo mollis, interdum sodales erat ultricies. Pellentesque in felis sem. Phasellus nunc tellus, suscipit rutrum risus quis, aucibus vel ipsumeu.</p>
+             
+
+        </div>
+        </div>
+  )
+}
+
+export default Daysermon
--- client/src/Pages/Sermons/Sermons.jsx
+++ client/src/Pages/Sermons/Sermons.jsx
@@ -3,10 +3,13 @@ import './sermons.css'
 import Hero from '../../Utilities/Hero/Hero'
 import './sermons.css'
 import sermonsImg from '../../assets/english.jpg'
+import Daysermon from './Daysermon'
 function Sermons() {
   return (
     <div className='overall-sermons-page-overall'>
         <Hero page="Sermons" imgsrc={sermonsImg}/>
+        <Daysermon/>
+
         </div>
   )
 }
