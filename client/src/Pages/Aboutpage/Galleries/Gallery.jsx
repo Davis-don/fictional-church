bddbe1956b1774cd7046772e31c3531ef10ed1e8
Davis-don <davismugoikou@gmail.com>
2024-10-30 15:17:47 +0300

    edited

--- client/src/Pages/Aboutpage/Galleries/Gallery.jsx
+++ client/src/Pages/Aboutpage/Galleries/Gallery.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './gallery.css'
-import Gallerycard from './Gallerycard';
+import Gallerycardcomponent from './Gallerycardcomponent'
 import galleryData from './dummydata'
 
 function Gallery() {
@@ -9,7 +9,7 @@ function Gallery() {
         <h1>Gallery</h1>
         <div className="gallery-cards-overall">        
     {
-        galleryData.map((content)=><Gallerycard image={content.image} date={content.date} title={content.title} description={content.description}/>)
+        galleryData.map((content)=><Gallerycardcomponent image={content.image} date={content.date} title={content.title} description={content.description}/>)
     }
     </div>
         </div>
---
+++ client/src/Pages/Aboutpage/Galleries/Gallerycardcomponent.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './gallerycard.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const centerText = {
+    textAlign:"center"
+    }
+    
+function Gallerycardcomponent({image,date,title,description}) {
+  return (
+    <div className='gallery-card card'>
+    <div className="image-gallery-item">
+    <img src={image} alt='gallery image'/>
+    </div>
+    <h3 style={centerText} className='text-secondary container-fluid'>{date}</h3>
+    <h4 style={centerText}>{title}</h4>
+    <p style={centerText} className='text-secondary fs-5'>{description}</p>
+    </div>
+  )
+}
+
+export default Gallerycardcomponent
