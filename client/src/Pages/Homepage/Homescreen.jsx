edc03b22844a8c15c4200972f214ff5dedab68b2
Davis-don <davismugoikou@gmail.com>
2024-11-11 12:10:11 +0300

    edietd carosel

--- client/src/Pages/Homepage/Homescreen.jsx
+++ client/src/Pages/Homepage/Homescreen.jsx
@@ -2,7 +2,6 @@ import React, { useRef } from 'react';
 import './Homepage.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Carosel from '../../components/Carosel/Carosel';
-import { FaArrowDown } from "react-icons/fa";
 import Abouthomepg from '../../components/Aboutsection/Abouthomepg';
 import Countercards from '../../components/Aboutsection/Countercards';
 import Massorder from '../../components/Massorder/Massorder';
@@ -12,24 +11,17 @@ function Homescreen() {
   const aboutSectionRef = useRef(null);
 
 
-  const scrollToAboutSection = () => {
-    if (aboutSectionRef.current) {
-      aboutSectionRef.current.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-
   return (
     <div className='overall-homescreen-container'>
       {/* Start of hero section */}
       <div className="homepage-hero-section">
+        <div className="on-carosel-opacity-regulator">
+          {/* Places black overall opacity on carosel */}
+        </div>
         <div className="carosel-container">
           <Carosel />
         </div>
 
-        
-        <div className="scroll-btn" onClick={scrollToAboutSection}>
-          <FaArrowDown className='actual-btn-arrow' />
-        </div>
       </div>
       {/* End of hero section */}
    
