39825a6df3ed3e8d4cc1ee09372d456c28e30e97
Davis-don <davismugoikou@gmail.com>
2024-11-12 12:34:27 +0300

    added link to on sidebar buttons

--- client/src/components/Layout/sidebar/sidebar.jsx
+++ client/src/components/Layout/sidebar/sidebar.jsx
@@ -3,7 +3,9 @@ import './sidebar.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { IoIosClose } from "react-icons/io";
 import useSideStateStore from '../../../Store/sidebarState';
+import { useNavigate } from 'react-router-dom';
 function Sidebar() {
+  const Navigate= useNavigate()
   const toggleSideBar = useSideStateStore((state)=>state.sidebarToggle)
   return (
     <div className='overall-sidebar-container'>
@@ -12,12 +14,12 @@ function Sidebar() {
       </div>
       <div className="nav-links-holder-div">
         <ul className='list-unstyled'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Sermons</li>
-          <li>Events</li>
-          <li>Churches</li>
-          <li>Contact</li>
+          <li onClick={()=>{Navigate("/");toggleSideBar()}}>Home</li>
+          <li onClick={()=>{Navigate("/about");toggleSideBar()}}>About</li>
+          <li onClick={()=>{Navigate("/sermons");toggleSideBar()}}>Sermons</li>
+          <li onClick={()=>{Navigate("/events");toggleSideBar()}}>Events</li>
+          <li onClick={()=>{Navigate("/churches");toggleSideBar()}}>Churches</li>
+          <li onClick={()=>{Navigate("/contact");toggleSideBar()}}>Contact</li>
         </ul>
         </div>
         </div>
